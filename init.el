1e4976682ab160c27f7865e7be77a75247d4e536
issy-kn <s1190205@gmail.com>
2015-08-21 23:36:07 +0900

    dot

---
+++ init.el
@@ -0,0 +1,139 @@
+;; 起動時のメッセージを非表示
+(setq inhibit-startup-message t)
+
+;; C-kで行全体を削除
+(setq kill-whole-line t)
+
+;; beepを消す
+(defun my-bell-function ()
+  (unless (memq this-command
+        '(isearch-abort abort-recursive-edit exit-minibuffer
+              keyboard-quit mwheel-scroll down up next-line previous-line
+              backward-char forward-char))
+    (ding)))
+(setq ring-bell-function 'my-bell-function)
+
+;; \C-aでインデントを飛ばした行頭に移動
+(defun beginning-of-indented-line (current-point)
+  "インデント文字を飛ばした行頭に戻る。ただし、ポイントから行頭までの間にインデント文字しかない場合は、行頭に戻る。"
+  (interactive "d")
+  (if (string-match
+       "^[ ¥t]+$"
+       (save-excursion
+         (buffer-substring-no-properties
+          (progn (beginning-of-line) (point))
+          current-point)))
+      (beginning-of-line)
+    (back-to-indentation)))
+
+(defun beginning-of-visual-indented-line (current-point)
+  "インデント文字を飛ばした行頭に戻る。ただし、ポイントから行頭までの間にインデント文 字しかない場合は、行頭に戻る。"
+  (interactive "d")
+  (let ((vhead-pos (save-excursion (progn (beginning-of-visual-line) (point))))
+        (head-pos (save-excursion (progn (beginning-of-line) (point)))))
+    (cond
+     ;; 物理行の1行目にいる場合
+     ((eq vhead-pos head-pos)
+      (if (string-match
+           "^[ ¥t]+$"
+           (buffer-substring-no-properties vhead-pos current-point))
+          (beginning-of-visual-line)
+        (back-to-indentation)))
+     ;; 物理行の2行目以降の先頭にいる場合
+     ((eq vhead-pos current-point)
+      (backward-char)
+      (beginning-of-visual-indented-line (point)))
+     ;; 物理行の2行目以降の途中にいる場合
+     (t (beginning-of-visual-line)))))
+
+(global-set-key "\C-a" 'beginning-of-visual-indented-line)
+(global-set-key "\C-e" 'end-of-visual-line)
+
+;; 対応するカッコをハイライト
+(show-paren-mode 1)
+
+;; カーソル行をハイライト
+(defface hlline-face
+  '((((class color)
+      (background dark))
+     (:background "dark slate gray"))
+    (((class color)
+      (background light))
+     (:background "ForestGreen"))
+    (t
+     ()))
+  "*Face used by hl-line.")
+(setq hl-line-face 'hlline-face)
+(global-hl-line-mode)
+
+; バックアップデータを作らない
+(setq make-backup-files nil)
+
+; 行番号
+(global-linum-mode t)
+
+;; Tabの代わりにスペースでインデント
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4 indent-tabs-mode nil)
+
+;; "yes or no"を"y or n"に
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(require 'package)
+(setq package-archives
+      (append '(("marmalade" . "http://marmalade-repo.org/packages/")
+                ("melpa" . "http://melpa.milkbox.net/packages/"))
+              package-archives))
+(package-initialize)
+
+;; for python (http://hamukazu.com/2014/12/05/setting-emacs-for-python/)
+(add-hook 'python-mode-hook
+          (lambda ()
+            (define-key python-mode-map (kbd "\C-m") 'newline-and-indent)
+            (define-key python-mode-map (kbd "RET") 'newline-and-indent)))
+
+(yas-global-mode t)
+
+(require 'py-autopep8)
+(add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
+
+(require 'auto-complete-config)
+(ac-config-default)
+
+(require 'tramp-cmds)
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+     ; Make sure it's not a remote buffer or flymake would not work
+     (when (not (subsetp (list (current-buffer)) (tramp-list-remote-buffers)))
+      (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                         'flymake-create-temp-inplace))
+             (local-file (file-relative-name
+                          temp-file
+                          (file-name-directory buffer-file-name))))
+        (list "pyflakes" (list local-file)))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pyflakes-init)))
+ 
+(add-hook 'python-mode-hook
+          (lambda ()
+            (flymake-mode t)))
+
+;; Color
+(if window-system (progn
+    (set-background-color "Black")
+    (set-foreground-color "LightGray")
+    (set-cursor-color "Gray")
+    (set-frame-parameter nil 'alpha 80) ;透明度
+    ))
+
+;; for C++
+(setq c-tab-always-indent t)
+(setq c-hungry-delete-key t)
+(setq c-basic-offset 4)
+(add-hook 'c-mode-common-hook
+          '(lambda ()
+             ;; センテンスの終了である ';' を入力したら、自動改行+インデント
+;;             (c-toggle-auto-hungry-state 1)
+             ;; RET キーで自動改行+インデント
+             (define-key c-mode-base-map "\C-m" 'newline-and-indent)
+))
